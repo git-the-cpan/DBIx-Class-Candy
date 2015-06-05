980862286c62603cdaee3474b85443a8c3ca94d2
Arthur Axel "fREW" Schmidt <frioux+cpan@gmail.com>
2015-06-05 06:06:01 +0000

    Import of FREW/DBIx-Class-Candy-0.004000.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Candy
    gitpan-cpan-version:      0.004000
    gitpan-cpan-path:         FREW/DBIx-Class-Candy-0.004000.tar.gz
    gitpan-cpan-author:       FREW
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Candy/Exports.pm
+++ lib/DBIx/Class/Candy/Exports.pm
@@ -1,5 +1,5 @@
 package DBIx::Class::Candy::Exports;
-$DBIx::Class::Candy::Exports::VERSION = '0.003001';
+$DBIx::Class::Candy::Exports::VERSION = '0.004000';
 # ABSTRACT: Create sugar for your favorite ORM, DBIx::Class
 
 use strict;
